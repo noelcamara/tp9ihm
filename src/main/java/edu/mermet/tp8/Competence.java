be82e7098c43bec6cfd7e01ff6d29e4037a5844b
ihaddaden billal <billal_ihaddaden@outlook.fr>
2020-04-03 13:59:10 +0200

    Ajout de la classe Competence

---
+++ src/main/java/edu/mermet/tp8/Competence.java
@@ -0,0 +1,15 @@
+package edu.mermet.tp8;
+
+public class Competence {
+	private Double competence;
+	public Competence(Double competence) {
+		this.competence=competence;
+	}
+	public Double getCompetence() {
+		return competence;
+	}
+	public void setCompetence(Double competence) {
+		this.competence = competence;
+	}
+	
+}
