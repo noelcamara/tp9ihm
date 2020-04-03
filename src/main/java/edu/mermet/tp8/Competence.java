9c3543b1aec7346cd8fddd15c5ed039f9b7f90c1
ihaddaden billal <billal_ihaddaden@outlook.fr>
2020-04-03 14:18:56 +0200

    ajout du constructeur par defaut pour un nouveau user et deux methode

--- src/main/java/edu/mermet/tp8/Competence.java
+++ src/main/java/edu/mermet/tp8/Competence.java
@@ -1,15 +1,48 @@
 package edu.mermet.tp8;
-
+/**
+ * 
+ * @author Billal Ihaddaden
+ *
+ */
 public class Competence {
 	private Double competence;
+	/**
+	 * Constructeur par default (le cas d'un nouveau joueur)
+	 */
+	public Competence() {
+		this.competence=(double) 1;
+	}
+	/**
+	 * Constructeur avec paramettre (creer un nouvelle competence a partir d'une donnee)
+	 * @param competence
+	 */
 	public Competence(Double competence) {
-		this.competence=competence;
-	}
-	public Double getCompetence() {
-		return competence;
-	}
-	public void setCompetence(Double competence) {
 		this.competence = competence;
 	}
 	
+	/**
+	 * 
+	 * @return competence du joueur
+	 */
+	public Double getCompetence() {
+		return competence;
+	}
+	
+	/**
+	 * Ajout de la competence de 0.1 pour chaque utilisation de quitter jusqu'a ce que la competence egale a 4
+	 */
+	public void AjoutComQuitter() {
+		if(competence<=4) {
+			competence=+0.1;
+		}
+	}
+	/**
+	 * ajout de la competence avec 0.2 pour l'utilisation du diaporama apres avoir depasser 2 et s'arrete en atteignant la competence 8 
+	 */
+	public void AjoutComDiap() {
+		if(competence>=2 && competence<=8) {
+			competence=+0.2;
+		}
+	}
+
 }
